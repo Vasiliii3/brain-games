6cc6b7edfc76676f1da0307076943fb12e38b1d7
Vasilii <petgga@gmail.com>
2022-06-08 21:21:26 +0300

    fix name funct even_number

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,11 +1,11 @@
 from brain_games.games.logic import generate_number
 
 
-def even_number(number: int) -> bool:
+def is_even_number(number: int) -> bool:
     return number % 2 == 0
 
 
 def play_even():
     number = generate_number(min_number=1, max_number=50)
-    answer = "yes" if even_number(number) else "no"
+    answer = "yes" if is_even_number(number) else "no"
     return number, answer, str
