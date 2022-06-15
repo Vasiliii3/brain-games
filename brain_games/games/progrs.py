f8a616c91db9a3f1095315418a72b57dd28bd5e3
Vasilii <petgga@gmail.com>
2022-06-15 21:51:56 +0300

    fix game progs

--- brain_games/games/progrs.py
+++ brain_games/games/progrs.py
@@ -7,15 +7,17 @@ DESCRIPTION = 'What number is missing in the progression?'
 def generate_progressions():
     number_start = random.randint(1, 50)
     step = random.randint(-10, 10)
-    a_progressions = [number_start, ]
+    a_progressions = []
     elements_replacement = random.randint(0, ELEMENTS_PROGRESSION - 1)
     last_elements = number_start
-    for x in range(1, ELEMENTS_PROGRESSION):
+    for x in range(ELEMENTS_PROGRESSION):
         new_elements = last_elements + step
         last_elements = new_elements
         a_progressions.append(new_elements)
     return a_progressions, elements_replacement
 
+
+# корректировка т.к длина длина списка начинается с 0
 
 def generate_question_answer():
     progression, element = generate_progressions()
